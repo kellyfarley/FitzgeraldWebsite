786305583898a2834ad1eef969bc62fb55fa423d
Kelly Farley <farleykc@gmail.com>
2016-05-23 11:46:20 -0500

    called on class

--- controller.rb
+++ controller.rb
@@ -9,6 +9,13 @@ post '/' do
 	writing = Text.new
 	writing.genre = params[:genre]
 	writing.text = params[:text]
-
-	erb :stats
+	writing.stats
+	writing.repeats
+	writing.weakwords
+	writing.passivevoice
+	writing.secondperson
+	writing.firstperson
+	writing.extreme
+	writing.contractions
+	writing.numbers
 end
