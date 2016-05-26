76ab0ec479a0c889b34cc86702fbc4c683125bc5
Kelly Farley <farleykc@gmail.com>
2016-05-26 08:33:27 -0500

    completed

--- controller.rb
+++ controller.rb
@@ -6,9 +6,7 @@ get '/' do
 end
 
 post '/' do
-	writing = Text.new
-	writing.genre = params[:genre]
-	writing.text = params[:text]
+	writing = Text.new(params[:genre], params[:text])
 	writing.stats
 	writing.repeats
 	writing.weakwords
@@ -18,4 +16,28 @@ post '/' do
 	writing.extreme
 	writing.contractions
 	writing.numbers
+
+	@genre = writing.genre
+
+	@wordcount = writing.wordcount
+	@sentencecount = writing.sentencecount
+
+	@sortedrepeats = writing.sortedrepeats
+
+	@goodtimes = writing.goodtimes
+	@verytimes = writing.verytimes
+	@reallytimes = writing.reallytimes
+	@gettimes = writing.gettimes
+	@saidtimes = writing.saidtimes
+	@stufftimes = writing.stufftimes
+	@thingtimes = writing.thingtimes
+
+	@passivetimes = writing.passivetimes
+	@istimes = writing.istimes
+	@wastimes = writing.wastimes
+	@weretimes = writing.weretimes
+
+	#how to address aspects of formal essay?
+
+	erb :stats
 end
